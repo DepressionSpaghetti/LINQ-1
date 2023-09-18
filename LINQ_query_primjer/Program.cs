37d9fc36f3e60db2f2577336dcf180f037b6722b
DepressionSpaghetti <102665381+DepressionSpaghetti@users.noreply.github.com>
2023-09-18 10:50:16 +0200

    Add files via upload

---
+++ LINQ_query_primjer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_query_primjer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // string collection
+            List<string> strings = new List<string>()
+            {
+                "C# tutorials",
+                "VB.NET Tutorials",
+                "Learn C++",
+                "MVC Tutorials" +
+                "Java"
+            };
+
+            // LINQ query syntax
+            var result = from s in strings
+                         where s.Contains("Tutorials")
+                         select s;
+
+            foreach(var str in result) Console.WriteLine(str);
+
+            Console.ReadKey();
+        }
+    }
+}
